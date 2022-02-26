0a5b4e3335dacc14e5a35359a4268fdca3c423ce
cr <cr@crdeMacBook-Pro.local>
2022-02-26 22:29:23 +0800

    map first

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -25,6 +25,14 @@ export const OrderDetail = (props) => {
             }
         })
     }
+    const renderMap = () => {
+        new BMap.Map("orderDetailMap", { enableMapClick: false });
+        // 创建地图实例  
+        var point = new BMap.Point(116.404, 39.915);
+        // 创建点坐标  
+        map.centerAndZoom(point, 15);
+        // 初始化地图，设置中心点坐标和地图级别 
+    }
 
     return (
 
